60bb8d13fc49d98b2dfdf1a2ce646698ff6d7915
Horea Oros <horea.oros@gmail.com>
2023-02-02 12:31:43 +0200

    add day 14 and 15

--- 14.py
+++ 14.py
@@ -7,15 +7,15 @@ def part1():
     elf2 = 1
 
     for i in range(input + 10):
-        if i % 1000 == 0:
-            print(i)
+        # if i % 1000 == 0:
+        #     print(i)
         receipt = receipt + [ ord(c) - ord('0') for c in str(receipt[elf1] + receipt[elf2]) ]
         elf1 = (elf1 + 1 + receipt[elf1]) % len(receipt)
         elf2 = (elf2 + 1 + receipt[elf2]) % len(receipt)
         #print(receipt)
     print(''.join(map(str,receipt[input:input + 10])))
 
-#part1()
+part1()
 
 def part2():
     input = '236021'
