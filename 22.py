df48284c5697a8b160e4932585cb1624d84164c9
Horea Oros <horea.oros@gmail.com>
2023-02-13 20:02:36 +0200

    add day 22 solution

--- 22.py
+++ 22.py
@@ -1,3 +1,4 @@
+from collections import deque
 import numpy as np
 depth = 5913
 target = (8,701)
@@ -24,18 +25,20 @@ def regionType(x, y):
         case 2:
             return 'N' # narrow
         
-ROWS = target[1] + 1
-COLS = target[0] + 1
+# ROWS = target[1] + 1
+# COLS = target[0] + 1
+ROWS = target[1] +1000
+COLS = target[0] +1000
 geoIndex = np.zeros((ROWS, COLS), dtype = np.int64)
 eroLevel = np.zeros((ROWS, COLS), dtype = np.int64)
 type = np.zeros((ROWS, COLS), dtype = np.int64)
 
 geoIndex[0, 0] = 0
-geoIndex[ROWS - 1, COLS - 1] = 0
+geoIndex[target[1], target[0]] = 0
 
 for i in range(ROWS):
     for j in range(COLS):
-        if (i == 0 and j == 0) or (i == ROWS - 1 and j == COLS - 1):
+        if (i == 0 and j == 0) or (i == target[1] and j == target[0]):
             geoIndex[i, j] = 0
         elif i == 0:
             geoIndex[i, j] = j * 16807
@@ -58,7 +61,98 @@ for i in range(ROWS):
 #     print()
 
 riskLevel = 0
-for i in range(ROWS):
-    for j in range(COLS):
+for i in range(target[1] + 1):
+    for j in range(target[0] + 1):
         riskLevel += type[i, j]
 print(riskLevel)
+
+
+targetPosition = (target[1], target[0])
+ # (0, 0) == neither, (1, 0) torch, (0, 1) climbing
+torch = 'torch'
+climbing = 'climbing'
+neither = 'neither'
+equipment = torch
+
+
+time = 0
+
+
+import heapq
+stateQ = (time, (0, 0), equipment)
+
+Q = [stateQ]
+
+best = dict() # (x, y, cannot) : minutes
+
+target = ((target[1], target[0]),torch)
+
+while Q:
+    state = heapq.heappop(Q)
+    time, currentPosition, equipment = state
+    
+    best_key = (currentPosition, equipment)
+    if best_key in best and best[best_key] <= time:
+        continue
+    best[best_key] = time
+    if best_key == target:
+        print(time)
+        break
+
+    i, j = currentPosition
+    #change equipment:
+    if type[i, j] == 0: # rocky
+        if equipment == torch:
+            newEquipment = climbing
+        elif equipment == climbing:
+            newEquipment = torch
+    elif type[i, j] == 1: # wet
+        if equipment == climbing:
+            newEquipment = neither
+        elif equipment == neither:
+            newEquipment = climbing
+    elif type[i, j] == 2: # narrow
+        if equipment == torch:
+            newEquipment = neither
+        elif equipment == neither:
+            newEquipment = torch
+    newStateWithChngEquipment = (time + 7, (i, j), newEquipment)
+    heapq.heappush(Q, newStateWithChngEquipment)
+
+    #change position
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, 1, -1]
+    for t in range(4):
+        newi = i + dx[t]
+        newj = j + dy[t]
+
+        if newi >= 0 and newj >= 0 and newi < ROWS and newj < COLS:
+            newStateWithSameEquipment = (time + 1, (newi, newj), equipment)
+
+
+            okSame = False
+            okChng = False
+            if equipment == torch:
+                if type[newi, newj] == 0 or type[newi, newj] == 2: # rocky or narrow
+                    okSame = True
+            elif equipment == climbing:
+                if type[newi, newj] == 0 or type[newi, newj] == 1: # rocky or wet
+                    okSame = True
+            elif equipment == neither:
+                if type[newi, newj] == 1 or type[newi, newj] == 2: # wet or narrow
+                    okSame = True
+            
+            if newEquipment == torch:
+                if type[newi, newj] == 0 or type[newi, newj] == 2: # rocky or narrow
+                    okChng = True
+            elif newEquipment == climbing:
+                if type[newi, newj] == 0 or type[newi, newj] == 1: # rocky or wet
+                    okChng = True
+            elif newEquipment == neither:
+                if type[newi, newj] == 1 or type[newi, newj] == 2: # wet or narrow
+                    okChng = True
+            
+            if okSame:
+                heapq.heappush(Q, newStateWithSameEquipment)
+            
+
